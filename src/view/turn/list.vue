ce6a7b9c9be8950a6b94278f056dcaddb4cb602b
wxlmm <921223971@qq.com>
2018-01-11 18:32:02 +0800

    修复下拉刷新和上拉加载

--- src/view/turn/list.vue
+++ src/view/turn/list.vue
@@ -1,20 +1,124 @@
 <template>
     <div class="list">
+      <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
         <div class="list-all">
             <v-card v-for="subject in subjects" :subject="subject" :key="subject.id"></v-card>
         </div>
+        <div class="msg" v-show="this.allLoaded">到底啦~</div>
+      </mt-loadmore>
     </div>
 </template>
 
 <script>
+import { Indicator, Toast } from 'mint-ui'
+import { mapState } from 'vuex'
 import Card from './../../components/card'
+import * as type from './../../store/modules/type'
 
 export default {
   name: 'List',
+  data () {
+    return {
+      page: 0,
+      loading: false,
+      allLoaded: false,
+      bottomStatus: ''
+    }
+  },
   components: {
     'v-card': Card
   },
-  props: ['tabName', 'subjects']
+  props: ['tabName'],
+  computed: mapState({
+    subjects (state) {
+      return state.topics.topics.data
+    }
+  }),
+  mounted () {
+    this.fetch(this.tabName, 0, 20)
+    this.page = 1
+    console.log(`mounted`)
+  },
+  watch: {
+    tabName: function (newtabName) {
+      this.allLoaded = false
+      this.bottomStatus = 'pull'
+      this.$store.dispatch(type.CLEAR_STATE_DATA)
+      this.page = 1
+      this.fetch(this.tabName, 0, 20)
+    }
+  },
+  methods: {
+    fetch (tab, page, limit) {
+      this.$store.dispatch(type.FETCH_TOPICS, {
+        tab: this.tabName,
+        page,
+        limit
+      })
+    },
+    fetchData (val) {
+      this.$store.dispatch(type.CLEAR_STATE_DATA)
+      this.page = 1
+      switch (val) {
+        case 'all':
+          this.fetch('all', 0, 20)
+          break
+        case 'good':
+          this.fetch('good', 0, 20)
+          break
+        case 'weex':
+          this.fetch('weex', 0, 20)
+          break
+        case 'share':
+          this.fetch('share', 0, 20)
+          break
+        case 'ask':
+          this.fetch('ask', 0, 20)
+          break
+        case 'job':
+          this.fetch('job', 0, 20)
+          break
+      }
+    },
+    loadMore () {
+      if (this.subjects.length !== 0) {
+        if (this.subjects.length % 20 !== 0) {
+          Toast({
+            message: '没有更多数据了',
+            duration: 700
+          })
+        } else {
+          Indicator.open({
+            text: '加载中...',
+            spinnerType: 'fading-circle'
+          })
+          setTimeout(() => {
+            this.loading = true
+            this.page += 1
+            this.fetch(this.selected, this.page, 20)
+            this.loading = false
+            Indicator.close()
+          }, 1000)
+        }
+      }
+    },
+    loadTop () {
+      this.$store.dispatch(type.CLEAR_STATE_DATA)
+      for (let i = 0; i < this.page; i++) {
+        this.fetch(this.selected, i, 20)
+      }
+      this.$refs.loadmore.onTopLoaded()
+    },
+    loadBottom () {
+      this.page += 1
+      this.fetch(this.selected, this.page, 20)
+      if (this.subjects.length % 20 !== 0) {
+        this.allLoaded = true
+        this.bottomStatus = 'null'
+      }
+      this.$refs.loadmore.onBottomLoaded()
+    }
+  }
 }
 </script>
 
@@ -22,4 +126,31 @@ export default {
 .list
   width 100%
   height auto
+
+  .mint-loadmore
+    flex 1
+    width 100%
+    position fixed
+    top 110px
+    left 0
+    right 0
+    bottom 6px
+    overflow-y auto
+
+    .mint-loadmore-content
+      width 100%
+      height 100%
+      position relative
+      top 0
+      left 0
+      bottom: 0
+      overflow-y: auto
+
+      .msg
+        width: 100%
+        height: 60px
+        display: flex
+        background: #f0f8ff
+        padding: 20px 42% 0 42%
+        box-sizing: border-box
 </style>
--- src/view/turn/topics.vue
+++ src/view/turn/topics.vue
@@ -10,39 +10,37 @@
             <mt-tab-item id="job">招聘</mt-tab-item>
         </mt-navbar>
 
-    <mt-loadmore :top-method="loadTop" :bottom-all-loaded="allLoaded" ref="loadmore">
-        <mt-tab-container v-model="selected"
-            v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="70">
+        <!-- <mt-tab-container v-model="selected">
 
             <mt-tab-container-item id="all">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
             <mt-tab-container-item id="good">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
             <mt-tab-container-item id="weex">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
             <mt-tab-container-item id="share">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
             <mt-tab-container-item id="ask">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
             <mt-tab-container-item id="job">
-                <v-list :tabName="selected" :subjects="subjects"></v-list>
+                <v-list :tabName="selected"></v-list>
             </mt-tab-container-item>
 
-        </mt-tab-container>
-    </mt-loadmore>
+        </mt-tab-container> -->
 
+        <v-list :tabName="selected"></v-list>
     </div>
 </template>
 
 <script>
-import { Indicator, Toast } from 'mint-ui'
-import { mapState } from 'vuex'
-import * as type from './../../store/modules/type'
+// import { Indicator } from 'mint-ui'
+// import { mapState } from 'vuex'
+// import * as type from './../../store/modules/type'
 import Header from './../header'
 import List from './list'
 let selectedItemName = 'all'
@@ -51,104 +49,16 @@ export default {
   name: 'Topics',
   data () {
     return {
-      selected: selectedItemName,
-      // subjects: [],
-      page: 0,
-      loading: false,
-      allLoaded: false
+      selected: selectedItemName
     }
   },
   components: {
     'v-list': List,
     'v-header': Header
   },
-  computed: mapState({
-    subjects (state) {
-      return state.topics.topics.data
-    }
-  }),
   watch: {
     selected: function (newselected) {
       selectedItemName = newselected
-      Indicator.open({
-        text: '加载中...',
-        spinnerType: 'fading-circle'
-      })
-      this.fetchData(this.selected)
-      setTimeout(() => Indicator.close(), 300)
-    }
-  },
-  created () {
-    if (this.subjects.length === 0) {
-      Indicator.open({
-        text: '加载中...',
-        spinnerType: 'fading-circle'
-      })
-      this.fetch('all', 0, 20)
-      this.page = 1
-      setTimeout(() => Indicator.close(), 300)
-    }
-  },
-  methods: {
-    fetchData (val) {
-      this.$store.dispatch(type.CLEAR_STATE_DATA)
-      this.page = 1
-      switch (val) {
-        case 'all':
-          this.fetch('all', 0, 20)
-          break
-        case 'good':
-          this.fetch('good', 0, 20)
-          break
-        case 'weex':
-          this.fetch('weex', 0, 20)
-          break
-        case 'share':
-          this.fetch('share', 0, 20)
-          break
-        case 'ask':
-          this.fetch('ask', 0, 20)
-          break
-        case 'job':
-          this.fetch('job', 0, 20)
-          break
-      }
-    },
-    fetch (tab, page, limit) {
-      this.$store.dispatch(type.FETCH_TOPICS, {
-        tab: this.selected,
-        page,
-        limit
-      })
-    },
-    loadMore () {
-      if (this.subjects.length !== 0) {
-        if (this.subjects.length % 20 !== 0) {
-          Toast({
-            message: '没有更多数据了',
-            duration: 700
-          })
-        } else {
-          Indicator.open({
-            text: '加载中...',
-            spinnerType: 'fading-circle'
-          })
-          setTimeout(() => {
-            this.loading = true
-            this.page += 1
-            this.fetch(this.selected, this.page, 20)
-            this.loading = false
-            Indicator.close()
-          }, 1000)
-        }
-      }
-    },
-    loadTop () {
-      this.$store.dispatch(type.CLEAR_STATE_DATA)
-      for (let i = 0; i < this.page; i++) {
-        this.fetch(this.selected, i, 20)
-      }
-      this.$refs.loadmore.onTopLoaded()
     }
   }
 }
@@ -168,31 +78,14 @@ export default {
     left 0
     right 0
 
-  .mint-loadmore
-    flex 1
-    width 100%
-    position fixed
-    top 110px
-    left 0
-    right 0
-    bottom 56px
-    overflow-y auto
-
-    .mint-loadmore-content
-      width 100%
-      height 100%
-      position relative
-      top 0
-      left 0
-
-      .mint-tab-container
-        // flex: 1
-        width 100%
-        height 100%
-        position relative
-        top 0
-        left 0
-        overflow-y auto
+      // .mint-tab-container
+      //   flex: 1
+      //   width 100%
+      //   height 100%
+      //   position relative
+      //   top 0
+      //   left 0
+      //   overflow-y auto
 
 .mint-tab-item-label
   font-size 16px
